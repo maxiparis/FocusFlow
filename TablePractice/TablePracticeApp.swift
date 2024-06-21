20a8a3cdbc003e228a5aae27d4d5ca4efef85c6b
Max Paris <mp829@byu.edu>
2024-06-21 14:55:33 -0600

    Initial Commit

---
+++ TablePractice/TablePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablePracticeApp.swift
+//  TablePractice
+//
+//  Created by Maximiliano Paris Gaete on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TablePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
