ea20a7ef6a146db6cabdad9655075228804a8486
pohako3212 <pohako3212@cohodl.com>
2024-10-23 20:25:54 -0600

    init

--- TablePractice/model/Task.swift
+++ TablePractice/model/Task.swift
@@ -8,16 +8,23 @@
 import Foundation
 
 struct Task: Identifiable, Equatable, Decodable, Encodable {
-        
     var id = UUID()
     var title: String
     var timer: TimeTracked
+    var dueDate: Date // Add this line
     var completed: Bool = false
     
+    // Update the initializer to include dueDate
+    init(title: String, hours: Int, minutes: Int, dueDate: Date) {
+        self.title = title
+        self.timer = TimeTracked(hours: hours, minute: minutes)
+        self.dueDate = dueDate
+    }
+
     static func == (lhs: Task, rhs: Task) -> Bool {
         return lhs.title == rhs.title && lhs.timer.hours == rhs.timer.hours && lhs.timer.minute == rhs.timer.minute
     }
-    
+
     func generateTimerText() -> String {
         let hoursText = timer.hours < 10 ? "0\(timer.hours.description)" : timer.hours.description
         let minutesText = timer.minute < 10 ? "0\(timer.minute.description)" : timer.minute.description
@@ -54,3 +61,4 @@ enum TimerState: Codable {
 //        task.completed
 //    }
 //}
+
